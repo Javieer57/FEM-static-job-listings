0c16f27a9ba8e84b00f79e880f552d8e6d2d7725
Javier Eufracio <jangel@ia.com.mx>
2023-06-04 12:49:44 -0600

    feat: add 'remove filter' logic

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,12 +1,18 @@
+import useFilters from "@/hooks/useFilters";
 import { FilterLabel } from "./Labels";
 
 const Filters = ({ filters }) => {
+  const { removeFilter } = useFilters();
   return (
     <div className="col-start-1 w-full px-4 max-w-5xl mx-auto row-start-2 row-end-4">
       <div className="flex justify-between items-center gap-5 py-5 pl-5 pr-6 bg-white drop-shadow-xl rounded-md">
         <div className="flex gap-4 flex-wrap">
           {filters.map((filter) => (
-            <FilterLabel text={filter} key={filter} />
+            <FilterLabel
+              text={filter}
+              key={filter}
+              onClick={() => removeFilter(filter)}
+            />
           ))}
         </div>
         <span className="text-gray font-bold">Clear</span>
--- src/hooks/useFilters.js
+++ src/hooks/useFilters.js
@@ -4,14 +4,27 @@ import { AppContext } from "@/contexts/JobsContext";
 const useFilters = () => {
   const [app, setApp] = useContext(AppContext);
 
+  const { filters } = app;
+
   const addFilter = (filter) => {
     const filters = [...app.filters];
     if (!filters.includes(filter)) filters.push(filter);
     setApp((prev) => ({ ...prev, filters }));
   };
 
+  const removeFilter = (filter) => {
+    const filters = [...app.filters];
+    const index = filters.indexOf(filter);
+    if (index !== -1) {
+      filters.splice(index, 1);
+    }
+    setApp((prev) => ({ ...prev, filters }));
+  };
+
   return {
+    filters,
     addFilter,
+    removeFilter,
   };
 };
 
